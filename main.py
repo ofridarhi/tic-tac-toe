f3e0934cb3c8a8312fcbb2e69d045acfbb9ef1ad
User <ofridarhi12345@gmail.com>
2022-07-24 15:32:45 +0300

    e

---
+++ main.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+class Player:
+    def __init__(self,name,shape):
+        self.shape = shape
+        self.name = name
+
+    def get_row_from_input(self):
+        while True:
+            row = int(input("in what row do you want to put it? (1-3)"))
+            if row < 1 or row > 3:
+                print("the number must be in range")
+            else:
+                return row
+                break
+    def get_col_from_input(self):
+        while True:
+            col = int(input("in what col do you want to put it? (1-3)"))
+            if col < 1 or col > 3:
+                print("the number must be in range")
+            else:
+                return col
+                break
+
+
+class turn:
+    def __init__(self,player1,player2):
+        self.player1 = player1
+        self.player2 = player2
+        self.current_player = player1
+
+    def update_turn(self):
+        if self.current_player == self.player1:
+            self.current_player == self.player2
+        else:
+            self.current_player == self.player1
+
+    def get_current_player_shape(self):
+        return self.current_player.shape
+
+
+def main():
+    board = np.array([['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']])
+    print(board)
+    run = True
+    while run:
+        player1 = Player('1','X')
+        player2 = Player('2','O')
+
+        player1.get_row_from_input()
+
+if __name__ == '__main__':
+    main()
